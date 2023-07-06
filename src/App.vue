6e675838c16cc69fc217f41d79eb8d0096f7ba14
keiko233 <keiko23333@gmail.com>
2023-07-07 01:35:20 +0800

    feat: Initial Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-app>
-    <router-view />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" color="grey-lighten-5">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar color="primary" :elevation="0">
+      <v-app-bar-nav-icon @click="drawer = !drawer" icon="menu" />
+
+      <v-app-bar-title v-text="$t('name')" />
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+const drawer = ref(true);
+</script>
