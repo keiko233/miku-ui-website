51d137632c8b4a98b7063a52953747c0a469628d
Majokeiko <keiko23333@gmail.com>
2023-07-07 01:17:00 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue'
+      ]
+    }),
+    Components({
+      resolvers: []
+    })
+  ],
+})
