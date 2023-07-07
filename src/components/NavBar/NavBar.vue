8abadb698c9e861aacb713d67c46de2ee53d591e
keiko233 <keiko23333@gmail.com>
2023-07-07 18:54:44 +0800

    feat: add author info card on NavBar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,18 +1,32 @@
 <template>
-  <v-list>
-    <v-list-item v-for="(item, i) in items" :key="i" :value="item" active-color="primary" rounded="xl"
-      @click="click(item.codename)">
-      <template v-slot:prepend>
-        <v-icon :icon="item.icon" />
+  <div class="container">
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i" :value="item" active-color="primary" rounded="xl"
+        @click="click(item.codename)">
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon" />
+        </template>
+        <v-list-item-title v-text="item.device" />
+        <v-list-item-subtitle v-if="item.codename" v-text="item.codename" />
+      </v-list-item>
+    </v-list>
+
+    <v-card class="mx-4 mb-4" prepend-icon="mdi-github">
+      <template v-slot:title>
+        <div @click="openUrl('https://elaina.moe')">keiko233</div>
       </template>
-      <v-list-item-title v-text="item.device" />
-      <v-list-item-subtitle v-if="item.codename" v-text="item.codename"/>
-    </v-list-item>
-  </v-list>
+
+      <v-card-text>
+        <div @click="openUrl('https://github.com/keiko233/miku-ui-website')">Version: {{ packageJson.version }}</div>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { openUrl } from '../../utils/openUrl';
+import packageJson from '../../../package.json';
 
 const router = useRouter();
 
@@ -29,3 +43,13 @@ const click = (path?: string) => {
   else router.push('/devices/' + path);
 };
 </script>
+
+<style scoped lang="less">
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+</style>
